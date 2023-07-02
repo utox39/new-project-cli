d8fa7c39145161d85177a49888339237b81513df
utox39 <francescomoccaldi39@gmail.com>
2023-07-02 22:42:07 +0200

    improved the install.sh script

--- install.sh
+++ install.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
-UNAME=$(uname)
+CMD_EXIT_STATUS=$?
 
-check_if_already_installed () {
-    if [ "$UNAME" == "Linux" ]; then
-        if [ -f /usr/local/bin/new-project ]; then
-            echo "new-project is already installed"
-            exit 0
-        fi
-    elif [ "$UNAME" == "Darwin" ]; then
-        if [ -f ~/bin/new-project ]; then
-            echo "new-project is already installed"
-            exit 0
-        fi
-    fi
-}
-
-check_if_already_installed
+if [ -f /usr/local/bin/new-project ]; then
+  echo "new-project is already installed"
+  exit 0
+fi
 
 if [ -f ./requirements.txt ]; then
     # installing requirements.txt
-    pip3 install -r requirements.txt
+    if [ $CMD_EXIT_STATUS -eq 0 ]; then
+      pip3 install -r requirements.txt
+    else
+      exit 1
+    fi
 
     # creating new_project_cli_tool config folder
-    mkdir ~/.config/new_project_cli_tool
+    if [ $CMD_EXIT_STATUS -eq 0 ]; then
+      mkdir ~/.config/new_project_cli_tool
+    else
+      exit 1
+    fi
 
     # copying config file in config folder
-    if [ -f ./new_project_config.json ]; then
-        cp ./new_project_config.json ~/.config/new_project_cli_tool/new_project_config.json
-    else
-        echo "Could not find new_project_config.json file"
-        exit 1
+    if [ $CMD_EXIT_STATUS -eq 0 ]; then
+      if [ -f ./new_project_config.json ]; then
+          cp ./new_project_config.json ~/.config/new_project_cli_tool/new_project_config.json
+      else
+          echo "Could not find new_project_config.json file"
+          exit 1
+      fi
     fi
 
     # making new project.py executable and copying it into the bin folder
-    if [ -f ./new_project.py ]; then
-        cp ./new_project.py new-project
-        chmod +x ./new-project
-        if [ "$UNAME" == "Linux" ]; then
-            sudo mv ./new-project /usr/local/bin/new-project
-        elif [ "$UNAME" == "Darwin" ]; then
-            sudo mv ./new-project /usr/local/bin/new-project
-        fi
-    else
-        echo "Could not find new_project.py file"
-        exit 1
+    if [ $CMD_EXIT_STATUS -eq 0 ]; then
+      if [ -f ./new_project.py ]; then
+          cp ./new_project.py new-project
+          chmod +x ./new-project
+          sudo mv ./new-project /usr/local/bin/new-project
+      else
+          echo "Could not find new_project.py file"
+          exit 1
+      fi
     fi
 
-    echo "Installation completed successfully"
+    if [ $CMD_EXIT_STATUS -eq 0 ]; then
+      echo "Installation completed successfully"
+    fi
+
 else
     echo "Could not find requirements.txt file"
     exit 1
