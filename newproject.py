ea63c71ab4359284ecdae4864b99746041e0d1dc
utox39 <francescomoccaldi39@gmail.com>
2023-07-13 00:27:31 +0200

    Fix

--- newproject.py
+++ newproject.py
@@ -20,8 +20,7 @@ from rich.console import Console
 console = Console()
 
 # Config file
-# CONFIG_FILE: Final[str] = f"{Path.home()}/.config/newproject/newproject_config.json"
-CONFIG_FILE: Final[str] = "./newproject_config.json"
+CONFIG_FILE: Final[str] = f"{Path.home()}/.config/newproject/newproject_config.json"
 
 # Load json config file
 try:
