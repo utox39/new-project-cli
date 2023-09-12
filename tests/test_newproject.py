fd451562ec5d45b8e175f007bc512460286e7f9a
utox39 <francescomoccaldi39@gmail.com>
2023-09-12 17:25:56 +0200

    Fix

--- tests/test_newproject.py
+++ tests/test_newproject.py
@@ -14,6 +14,7 @@ from newproject.newproject import Check, NewProject
 class TestCheck(unittest.TestCase):
     def test_config_file_validator(self):
         # Create a sample config file and JSON schema for testing
+        print("- test_config_file_validator\n")
         config_file = {
             "key1": "value1",
             "key2": "value2"
@@ -36,6 +37,7 @@ class TestCheck(unittest.TestCase):
         print("OK\n----------")
 
     def test_dev_dir_check(self):
+        print("- test_dev_dir_check\n")
         # Create a temporary directory for testing
         with tempfile.TemporaryDirectory() as temp_dir:
             # Test an existing directory
@@ -48,6 +50,8 @@ class TestCheck(unittest.TestCase):
             print("OK\n----------")
 
     def test_path_check(self):
+        print("- test_path_check\n")
+
         with tempfile.TemporaryDirectory() as temp_dir:
             non_existing_dir = os.path.join(temp_dir, "non_existing")
             with self.assertRaises(SystemExit) as cm:
@@ -57,6 +61,8 @@ class TestCheck(unittest.TestCase):
             print("OK\n----------")
 
     def test_name_check(self):
+        print("- test_name_check\n")
+
         with self.assertRaises(SystemExit) as cm:
             Check.projects_path_check("invalid name")
         self.assertEqual(cm.exception.code, 2)
@@ -65,10 +71,11 @@ class TestCheck(unittest.TestCase):
 
 
 class TestNewProject(unittest.TestCase):
-    @patch('shutil.which', return_value='path/to/code')
+    @unittest.skipIf(which("code") is None, "Do not run if code is not installed")
+    @patch('shutil.which', return_value='code')
     @patch('subprocess.run')
     def test_open_in_ide_success(self, mock_run, mock_which):
-        # mock_which.return_value = which("code")
+        print("- test_open_in_ide_success\n")
 
         NewProject.open_in_ide('code', '/path/to/project')
 
@@ -78,19 +85,22 @@ class TestNewProject(unittest.TestCase):
         print("OK\n----------")
 
     @patch('shutil.which')
-    @patch('builtins.print')
-    def test_open_in_ide_command_not_found(self, mock_print, mock_which):
-        mock_which.return_value = None
+    def test_open_in_ide_command_not_found(self, mock_which):
+        print("- test_open_in_ide_command_not_found\n")
 
-        NewProject.open_in_ide('invalid_ide', '/path/to/project')
+        with patch('builtins.print') as mock_print:
+            mock_which.return_value = None
 
-        mock_print.assert_called_once_with('newproject: invalid_ide: ide command not found')
+            NewProject.open_in_ide('invalid_ide', '/path/to/project')
+
+            mock_print.assert_called_once_with('newproject: invalid_ide: ide command not found')
 
         print("OK\n----------")
 
     @patch('subprocess.run')
     @patch('shutil.which')
     def test_git_init_command_success(self, mock_which, mock_run):
+        print("- test_git_init_command_success\n")
         mock_which.return_value = which("git")
 
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -107,6 +117,7 @@ class TestNewProject(unittest.TestCase):
     @patch('subprocess.run')
     # @patch('builtins.print')  # Mock the print function
     def test_git_init_failure(self, mock_run):
+        print("- test_git_init_failure\n")
         with tempfile.TemporaryDirectory() as temp_dir:
             project_dir = os.path.join(temp_dir, 'test_project')
             content = 'sample_content'
