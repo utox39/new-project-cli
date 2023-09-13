e2b891d46edefce61a2fe0b0e5e217f826797b30
utox39 <francescomoccaldi39@gmail.com>
2023-09-13 13:57:08 +0200

    Minor changes

--- newproject/newproject.py
+++ newproject/newproject.py
@@ -216,9 +216,9 @@ class NewProject:
         )
         try:
             # MacOS and Linux
-            if sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
-                with console.status("[dodger_blue1]Generating...[/dodger_blue1]", spinner="aesthetic"):
-                    subprocess.run(["python3", "-m", "venv", f"{new_project_path}/venv"])
+            # if sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
+            with console.status("[dodger_blue1]Generating...[/dodger_blue1]", spinner="aesthetic"):
+                subprocess.run(["python3", "-m", "venv", f"{new_project_path}/venv"])
             # # Windows
             # elif sys.platform.startswith("win32"):
             #     with console.status("[dodger_blue1]Generating...[/dodger_blue1]", spinner="aesthetic"):
